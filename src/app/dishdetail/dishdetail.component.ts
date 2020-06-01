4961735c689299204d3b3f38776ab673aaf9919f
sheldon <sheldonhenriques2727@gmail.com>
2020-06-02 02:04:06 +0530

    Adding animations P2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,7 +3,7 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, expand } from '../animations/app.animation';
 
 import { Comment } from '../shared/comment';
 
@@ -16,17 +16,8 @@ import { Location } from '@angular/common';
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility(),
+    expand()
   ]
 })
 export class DishdetailComponent implements OnInit {
